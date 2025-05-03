f217077de6da58144c5e73d3adcd62a66b4c95c5
BarrakBM <barrak_mandani@live.com>
2025-05-03 16:54:15 +0300

    Inital commit

---
+++ src/main/kotlin/com/example/Banking_App/BankingAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Banking_App
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankingAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankingAppApplication>(*args)
+}
