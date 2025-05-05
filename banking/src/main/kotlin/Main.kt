e9f5abcf3ca22a4c2e77f85bb7f86fb49319346f
najlaasz <najlaa.alzabi@gmail.com>
2025-05-05 17:12:11 +0300

    adding authentication for banking

--- banking/src/main/kotlin/Main.kt
+++ banking/src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
 package com.example
 
-fun main() {
-    println("Hello World!")
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class banking
+fun main(args: Array<String>) {
+
+    runApplication<banking>(*args)
 }
