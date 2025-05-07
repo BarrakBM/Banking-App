d8478f7d2596d57633aca04f4c091343cf371b85
BarrakBM <barrak_mandani@live.com>
2025-05-07 17:13:47 +0300

    user can create group admin can add members to group admin can remove memeber from the grouo admin can de-activate group admin can pay for the group

--- banking/src/main/kotlin/Main.kt
+++ banking/src/main/kotlin/Main.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class banking
+class Banking
 fun main(args: Array<String>) {
 
-    runApplication<banking>(*args)
+    runApplication<Banking>(*args)
 }
